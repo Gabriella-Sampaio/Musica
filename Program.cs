4df5fe245ca8908048f51498c29f773a7c7b7ba4
Gabriella Sampaio <gabyy.t.s21@gmail.com>
2025-03-08 11:43:43 -0300

    Término

---
+++ Program.cs
@@ -0,0 +1,291 @@
+﻿//Escolha de cores: álbum da música, Fallen
+//
+
+Console.Clear();
+
+Console.BackgroundColor = ConsoleColor.White;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine("* Bring me to life - Evanescence *"); Thread.Sleep(2000);
+Console.ResetColor();
+
+Console.WriteLine(); 
+
+Console.Write("How can you see "); Thread.Sleep(2300);
+Console.WriteLine("into my eyes"); Thread.Sleep(2300);
+Console.WriteLine("Like open doors?"); Thread.Sleep(2800);
+Console.WriteLine("Leading you down "); Thread.Sleep(2300);
+Console.WriteLine("into my core"); Thread.Sleep(2300);
+Console.WriteLine("Where I've become so numb"); Thread.Sleep(2800);
+
+Console.WriteLine();
+
+Console.WriteLine("Without a soul"); Thread.Sleep(2800);
+Console.Write("My spirit's sleeping "); Thread.Sleep(2300);
+Console.WriteLine("somewhere cold"); Thread.Sleep(2300);
+Console.Write("Until you find it "); Thread.Sleep(2300);
+Console.WriteLine("there and lead"); Thread.Sleep(2300);
+Console.WriteLine("And back"); Thread.Sleep(2300);
+Console.Write("Home"); Thread.Sleep(1000);
+Console.Write("."); Thread.Sleep(1000);
+Console.Write("."); Thread.Sleep(1000);
+Console.WriteLine("."); Thread.Sleep(800);
+
+Console.WriteLine();
+Console.WriteLine();
+
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine("WAKE ME UP"); Thread.Sleep(1500); 
+Console.BackgroundColor = ConsoleColor.Cyan;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine("Wake me up inside"); Thread.Sleep(1500); 
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine("I CAN'T WAKE UP"); Thread.Sleep(1500); 
+Console.BackgroundColor = ConsoleColor.Cyan;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine("Wake me up inside"); Thread.Sleep(1500);
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine("SAVE MEEEEE"); Thread.Sleep(1500);
+Console.BackgroundColor = ConsoleColor.Cyan;
+Console.ForegroundColor = ConsoleColor.Black; 
+Console.WriteLine("Call my name and"); Thread.Sleep(1500);
+Console.WriteLine("Save me from the dark"); Thread.Sleep(2300);
+
+Console.WriteLine();
+
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine("WAKE ME UP"); Thread.Sleep(1500); 
+Console.BackgroundColor = ConsoleColor.Cyan;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine("Bid my blood to run"); Thread.Sleep(1500); 
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine("I CAN'T WAKE UP"); Thread.Sleep(1500); 
+Console.BackgroundColor = ConsoleColor.Cyan;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine("Before I come undone"); Thread.Sleep(1500);
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine("SAVE MEEEEE"); Thread.Sleep(1500);
+Console.BackgroundColor = ConsoleColor.Cyan;
+Console.ForegroundColor = ConsoleColor.Black; 
+Console.WriteLine("Save me from the"); Thread.Sleep(1500); 
+Console.Write("Nothing I've become"); Thread.Sleep(1000); 
+Console.Write("."); Thread.Sleep(1000);
+Console.Write("."); Thread.Sleep(1000);
+Console.WriteLine("."); Thread.Sleep(1500);
+Console.ResetColor();
+
+Console.WriteLine();
+Console.WriteLine();
+
+Console.Write("Now that I know "); Thread.Sleep(2300);
+Console.WriteLine("what I'm without"); Thread.Sleep(2300);
+Console.WriteLine("You can't just leave me"); Thread.Sleep(2500);
+Console.Write("Breathe "); Thread.Sleep(2300);
+Console.Write("into me "); Thread.Sleep(2300);
+Console.WriteLine("and make me real"); Thread.Sleep(2300);
+
+Console.WriteLine("Bring"); Thread.Sleep(1500);
+Console.WriteLine("Me"); Thread.Sleep(1500);
+Console.Write("To life"); Thread.Sleep(1500);
+Console.Write("."); Thread.Sleep(1000);
+Console.Write("."); Thread.Sleep(1000);
+Console.WriteLine("."); Thread.Sleep(1500);
+
+Console.WriteLine();
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("WAKE ME UP"); Thread.Sleep(1500); 
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("Wake me up inside"); Thread.Sleep(1500); 
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("I CAN'T WAKE UP"); Thread.Sleep(1500); 
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("Wake me up inside"); Thread.Sleep(1500);
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("SAVE MEEEEE"); Thread.Sleep(1500);
+Console.ForegroundColor = ConsoleColor.Cyan; 
+Console.WriteLine("Call my name and"); Thread.Sleep(1500);
+Console.WriteLine("Save me from the dark"); Thread.Sleep(2000);
+
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("WAKE ME UP"); Thread.Sleep(1500); 
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("Bid my blood to run"); Thread.Sleep(1500); 
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("I CAN'T WAKE UP"); Thread.Sleep(1500); 
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("Before I come undone"); Thread.Sleep(1500);
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("SAVE MEEEEE"); Thread.Sleep(1500);
+Console.ForegroundColor = ConsoleColor.Cyan; 
+Console.WriteLine("Save me from the"); Thread.Sleep(1500); 
+Console.Write("Nothing I've become"); Thread.Sleep(800); 
+Console.Write("."); Thread.Sleep(800);
+Console.Write("."); Thread.Sleep(800);
+Console.WriteLine("."); Thread.Sleep(800);
+Console.ResetColor();
+
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Cyan; 
+Console.Write("Bring "); Thread.Sleep(1500);
+Console.Write("me "); Thread.Sleep(1500);
+Console.Write("to "); Thread.Sleep(1500);
+Console.Write("life"); Thread.Sleep(1000);
+Console.ResetColor();
+
+Console.WriteLine();
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("I've been livin' a lie"); Thread.Sleep(2000);
+Console.WriteLine("There's nothing inside"); Thread.Sleep(2000);
+Console.ResetColor();
+
+Console.WriteLine();
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Cyan; 
+Console.Write("Bring "); Thread.Sleep(1500);
+Console.Write("me "); Thread.Sleep(1500);
+Console.Write("to "); Thread.Sleep(1500);
+Console.Write("life"); Thread.Sleep(800);
+Console.Write("."); Thread.Sleep(800);
+Console.Write("."); Thread.Sleep(800);
+Console.WriteLine("."); Thread.Sleep(800);
+Console.ResetColor();
+
+Console.WriteLine();
+Console.WriteLine();
+
+Console.WriteLine("Froooozen inside"); Thread.Sleep(2300);
+Console.WriteLine("Without your touch"); Thread.Sleep(2300);
+Console.Write("Without your love, "); Thread.Sleep(2300);
+Console.WriteLine("darling"); Thread.Sleep(2300);
+Console.WriteLine("Only you"); Thread.Sleep(2300);
+Console.WriteLine("Are the life"); Thread.Sleep(2300);
+Console.WriteLine("Among"); Thread.Sleep(1200);
+Console.Write("The dead"); Thread.Sleep(1200);
+Console.Write("."); Thread.Sleep(800);
+Console.Write("."); Thread.Sleep(800);
+Console.WriteLine("."); Thread.Sleep(800);
+
+Console.WriteLine();
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("All this time, I can't believe I couldn't see"); Thread.Sleep(2000);
+Console.WriteLine("Kept in the dark, but you were there in front of me"); Thread.Sleep(2000);
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("I've been sleeping a thousand years, it seems"); Thread.Sleep(2000);
+Console.WriteLine("Got to open my eyes to everything"); Thread.Sleep(2000);
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.Write("Without a thought "); Thread.Sleep(800);
+Console.Write("Without a voice "); Thread.Sleep(800);
+Console.WriteLine("Without a soul"); Thread.Sleep(800);
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("Don't let me die here"); Thread.Sleep(800);
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("There must be something more"); Thread.Sleep(800);
+
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.Write("Bring "); Thread.Sleep(1500);
+Console.Write("me "); Thread.Sleep(1500);
+Console.Write("to "); Thread.Sleep(1500);
+Console.Write("life"); Thread.Sleep(800);
+Console.Write("."); Thread.Sleep(800);
+Console.Write("."); Thread.Sleep(800);
+Console.WriteLine("."); Thread.Sleep(800);
+Console.ResetColor();
+
+Console.WriteLine();
+Console.WriteLine();
+
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine("WAKE ME UP"); Thread.Sleep(1500); 
+Console.BackgroundColor = ConsoleColor.Cyan;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine("Wake me up inside"); Thread.Sleep(1500); 
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine("I CAN'T WAKE UP"); Thread.Sleep(1500); 
+Console.BackgroundColor = ConsoleColor.Cyan;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine("Wake me up inside"); Thread.Sleep(1500);
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine("SAVE MEEEEE"); Thread.Sleep(1500);
+Console.BackgroundColor = ConsoleColor.Cyan;
+Console.ForegroundColor = ConsoleColor.Black; 
+Console.WriteLine("Call my name and"); Thread.Sleep(1500);
+Console.WriteLine("Save me from the dark"); Thread.Sleep(2300);
+
+Console.WriteLine();
+
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine("WAKE ME UP"); Thread.Sleep(1500); 
+Console.BackgroundColor = ConsoleColor.Cyan;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine("Bid my blood to run"); Thread.Sleep(1500); 
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine("I CAN'T WAKE UP"); Thread.Sleep(1500); 
+Console.BackgroundColor = ConsoleColor.Cyan;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine("Before I come undone"); Thread.Sleep(1500);
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine("SAVE MEEEEE"); Thread.Sleep(1500);
+Console.BackgroundColor = ConsoleColor.Cyan;
+Console.ForegroundColor = ConsoleColor.Black; 
+Console.WriteLine("Save me from the"); Thread.Sleep(1500); 
+Console.Write("Nothing I've become"); Thread.Sleep(1000); 
+Console.Write("."); Thread.Sleep(1000);
+Console.Write("."); Thread.Sleep(1000);
+Console.WriteLine("."); Thread.Sleep(1500);
+Console.ResetColor();
+
+Console.WriteLine();
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Cyan; 
+Console.Write("Bring "); Thread.Sleep(1500);
+Console.Write("me "); Thread.Sleep(1500);
+Console.Write("to "); Thread.Sleep(1500);
+Console.Write("life"); Thread.Sleep(800);
+Console.Write("."); Thread.Sleep(800);
+Console.Write("."); Thread.Sleep(800);
+Console.WriteLine("."); Thread.Sleep(800);
+Console.ResetColor();
+
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("I've been livin' a lie"); Thread.Sleep(2000);
+Console.WriteLine("There's nothing inside"); Thread.Sleep(2000);
+Console.ResetColor();
+
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Cyan; 
+Console.Write("Bring "); Thread.Sleep(1500);
+Console.Write("me "); Thread.Sleep(1500);
+Console.Write("to "); Thread.Sleep(1500);
+Console.Write("liiiiiiiiiiiiiiiife"); Thread.Sleep(2300);
+Console.Write("."); Thread.Sleep(1000);
+Console.Write("."); Thread.Sleep(1000);
+Console.WriteLine("."); Thread.Sleep(1000);
+
+Console.ResetColor();
